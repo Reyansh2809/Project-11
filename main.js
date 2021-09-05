439b1269f4fa8040d6db7104273e3d8dd7e6ed32
Reyansh2809 <77835541+Reyansh2809@users.noreply.github.com>
2021-09-05 10:38:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload()
+{    
+}
+function setup()
+{
+canvas = createCanvas(300,300);
+canvas.center();
+}
+function draw()
+{
+}
+function take_snapshot()
+{
+save('realistic_filter_for_funny.png');
+}
